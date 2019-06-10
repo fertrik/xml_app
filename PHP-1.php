4ce4b98cf6293cdb2ba1a0e16eddd100dc9b9a8f
fertrik <51640321+fertrik@users.noreply.github.com>
2019-06-10 17:22:59 +0200

    Update PHP-1.php

--- PHP-1.php
+++ PHP-1.php
@@ -1,25 +1,30 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+	<title>Untitled</title>
+</head>
 
-
+<body>
 <?php
 
-$html = "";
+    $html = "";
 
-$url = "http://www.realbeta.net63.net/blog/rss?id=1";
-$xml = simplexml_load_file($url);
-for($i = 0; $i < 1; $i++){
+	$url = "http://rss.nytimes.com/services/xml/rss/nyt/Sports.xml";
+	$xml = simplexml_load_file($url);
+	for($i = 0; $i < 1; $i++){
   
-	$title = $xml->channel->item[$i]->title;
-	$link = $xml->channel->item[$i]->link;
-	$description = $xml->channel->item[$i]->description;
-	$pubDate = $xml->channel->item[$i]->pubDate;
+		$title = $xml->channel->item[$i]->title;
+		$link = $xml->channel->item[$i]->link;
+		$description = $xml->channel->item[$i]->description;
+		$pubDate = $xml->channel->item[$i]->pubDate;
+		
+		$html .= "<ul><li>Link: <a target='_blank' href='$link'><b>$title</b></a></li>"; 
+		$html .= "<li>Description: $description </li>"; 
+		$html .= "<li>Publication Date: $pubDate</li></ul>";
 	
-
-	$html .= "<a target='_blank' href='$link'><b>$title</b></a>"; // Title of post
-	$html .= "$description"; // Description
-	$html .= "<br />$pubDate<br /><br />"; // Date Published
 }
-echo "$html<br />";
-
+echo "$html";
 ?>
 
+</body>
+</html>
